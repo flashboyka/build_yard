12fb6019cf16408931b27c346984b00a5a43e973
master <master@linux-dima.atmpartners.loc>
2020-01-14 13:26:50 +0500

    Убрал закоментированный код

--- goods/api.py
+++ goods/api.py
@@ -1,6 +1,7 @@
 from tastypie.resources import ModelResource
-from tastypie.authorization import Authorization, DjangoAuthorization
+from tastypie.authorization import DjangoAuthorization
 from tastypie.authentication import BasicAuthentication
+
 from goods.models import Good
 
 
@@ -8,6 +9,5 @@ class GoodResource(ModelResource):
     class Meta:
         queryset = Good.objects.all()
         resource_name = 'goods'
-        #authorization = Authorization()
         authorization = DjangoAuthorization()
         authentication = BasicAuthentication()
--- orders/api.py
+++ orders/api.py
@@ -1,6 +1,7 @@
 from tastypie.resources import ModelResource
-from tastypie.authorization import Authorization, DjangoAuthorization
+from tastypie.authorization import DjangoAuthorization
 from tastypie.authentication import BasicAuthentication
+
 from orders.models import Order
 
 
@@ -8,6 +9,5 @@ class OrderResource(ModelResource):
     class Meta:
         queryset = Order.objects.all()
         resource_name = 'orders'
-        #authorization = Authorization()
         authorization = DjangoAuthorization()
         authentication = BasicAuthentication()
