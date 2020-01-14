5db27df6a02fd0debbfb4204ad909fcf09ca56f1
master <master@linux-dima.atmpartners.loc>
2020-01-14 13:07:25 +0500

    Добавил авторизацию для api В contragents, goods, orders

--- contragents/api.py
+++ contragents/api.py
@@ -1,5 +1,6 @@
 from tastypie.resources import ModelResource
-
+from tastypie.authorization import Authorization, DjangoAuthorization
+from tastypie.authentication import BasicAuthentication
 from contragents.models import Contragent
 
 
@@ -7,3 +8,6 @@ class ContragentResource(ModelResource):
     class Meta:
         queryset = Contragent.objects.all()
         resource_name = 'contragents'
+        #authorization = Authorization()
+        authorization = DjangoAuthorization()
+        authentication = BasicAuthentication()
--- orders/api.py
+++ orders/api.py
@@ -1,5 +1,6 @@
 from tastypie.resources import ModelResource
-
+from tastypie.authorization import Authorization, DjangoAuthorization
+from tastypie.authentication import BasicAuthentication
 from orders.models import Order
 
 
@@ -7,3 +8,6 @@ class OrderResource(ModelResource):
     class Meta:
         queryset = Order.objects.all()
         resource_name = 'orders'
+        #authorization = Authorization()
+        authorization = DjangoAuthorization()
+        authentication = BasicAuthentication()
